7d9df1fc2a6044777e972cbbf8c048f56fe9afb3
Anuj <work.anujtiwari27@gmail.com>
2025-12-24 00:56:47 +0530

    expense

---
+++ Python projects/expense.py
@@ -0,0 +1,25 @@
+# # Expense tracker 
+# list - expenses 
+# dict - category
+# Amount, category,
+
+
+print("Welcome to expense tracker!")
+amount = []
+category = []
+option = int(input("Select an option between 1-3: "))
+expenses = [
+    {category, amount}
+]
+
+if option == 1:
+    money = int(input("Enter the amount: "))
+    amount.append(money)
+    cat = input("Enter the expense category: ")
+    category.append(cat)
+
+elif option == 2:
+    print("Thanks for using expense tracker")
+    
+else:
+    print("Enter a correct input")
